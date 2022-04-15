3455f10bd3257f7ba3ba232022bb4598d157a7df
Tadashi <98858630+pizzlemank@users.noreply.github.com>
2022-04-15 02:03:00 +0000

    Update wih.py

--- wih.py
+++ wih.py
@@ -3,40 +3,6 @@
 # Made with Python 3.7...I think. I don't remember LOL
 # This is free software, use as you wish.
 # I probably won't maintain this, as I no longer use Discord.
-# Questions? Contact me: mtngithub@protonmail.com
-
-'''
-
-HOW TO USE THIS SCRIPT
-
-0. Generate your Discord bot and its token.
-    - Stuck? Use the following link to learn how.
-    - https://www.writebots.com/discord-bot-token/
-
-1. Install the latest version of Python on your system.
-
-2. Open your command line/terminal.
-    - For Windows users, use Win+R, type "cmd", then hit enter.
-    - Linux users...I don't think you need help, haha.
-
-3. Edit WIH.py in your preferred text editor. THIS STEP IS VERY IMPORTANT.
-    - Make sure to replace all the #your user ID here#s in the code with your user ID (not your tag)!
-    - Otherwise, the bot will not work properly :( so you better do it!
-    - Make sure to add your bot token too, otherwise it won't run at all!
-
-These next steps are all done within the terminal.
-
-4. Type in "pip install discord" or "pip3 install discord". 
-    - This installs the scripts used to be able to connect the bot to Discord.
-    - If this doesn't work, search engines and forums are your friends.
-
-5. Go to the folder that WIH.py is in. (Stuck? Look up the cd command how-to guides.)
-
-6. Type in "python WIH.py" or "python3 WIH.py" and hit enter.
-
-Have fun!
-
-'''
 
 import discord
 from discord.ext import commands, tasks
